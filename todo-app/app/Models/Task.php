9b90b8726c2a44b805426bf9e357a78ba9d3c8ac
Diogo <diogo.sanlez@trainee.inovcorp.com>
2025-09-22 12:22:45 +0100

    Model, controller, routes and migrations

--- todo-app/app/Models/Task.php
+++ todo-app/app/Models/Task.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
+    protected $fillable = [
+        'title',
+        'description',
+        'is_completed',
+        'due_date',
+        'priority',
+    ];
     
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- todo-app/database/migrations/2025_09_22_104731_create_tasks_table.php
+++ todo-app/database/migrations/2025_09_22_104731_create_tasks_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->boolean('is_completed')->default(false);
+            $table->date('due_date')->nullable();
+            $table->enum('priority', ['low', 'medium', 'high'])->default('medium');
             $table->timestamps();
         });
     }
